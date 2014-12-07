2e9c4e84c1dde4b906dd8f48cb9f3e5a00102257
Luigi Grilli <luigi.grilli@gmail.com>
2014-12-07 23:46:01 +0000

    Added named pipe server
    Added named pipe server (not implemented)

---
+++ FastCgi/NamedPipe/NamedPipeServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Pipes;
+
+namespace FastCgi.NamedPipes
+{
+    public class NamedPipeServer
+    {
+        private readonly NamedPipeServerStream _server;
+
+        public NamedPipeServer(string pipeName)
+        {
+            _server = new NamedPipeServerStream(pipeName);
+        }
+
+        private void Run()
+        {
+
+        }
+    }
+}
