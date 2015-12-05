4074b6c9e80d622d9f7ddc886c258189cc07c449
Luigi Grilli <luigi.grilli@gmail.com>
2015-12-05 10:50:38 +0000

    Code improvement

--- FastCgi.Loggers.Log4Net/Loggerfactory.cs
+++ FastCgi.Loggers.Log4Net/Loggerfactory.cs
@@ -1,20 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Grillisoft.FastCgi.Loggers.Log4Net
 {
 	[Serializable]
-    public class LoggerFactory : Grillisoft.FastCgi.ILoggerFactory
+    public sealed class LoggerFactory : ILoggerFactory
     {
-        public Grillisoft.FastCgi.ILogger Create(Type type)
+        public ILogger Create(Type type)
         {
             return new Logger(log4net.LogManager.GetLogger(type));
         }
 
-        public Grillisoft.FastCgi.ILogger Create(string name)
+        public ILogger Create(string name)
         {
             return new Logger(log4net.LogManager.GetLogger(name));
         }
